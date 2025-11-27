67c7eab29d80952fedb7fbdbd6ac49bdd0617238
rahulkale09 <rahkal1@gmail.com>
2025-11-27 15:59:32 +0530

    8th commit

--- table_of_number.py
+++ table_of_number.py
@@ -1,7 +1,9 @@
 #Table of a number
+def multi():
 
-i = int(input("Enter a number: "))
+    i = int(input("Enter a number: "))
 
-for j in range(1, 11):
-    print(i, 'X', j, '=', i * j)
+    for j in range(1, 11):
+        print(i, 'X', j, '=', i * j)
 
+multi()
